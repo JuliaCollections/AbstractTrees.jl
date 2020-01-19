b1a8760add66c17538723c02bf01160ed45adfc3
Jared Lumpe <mjlumpe@gmail.com>
2020-01-18 18:00:01 -0800

    Add module docstring

--- src/AbstractTrees.jl
+++ src/AbstractTrees.jl
@@ -1,3 +1,9 @@
+"""
+This package is intended to provide an abstract interface for working
+with tree structures.
+Though the package itself is not particularly sophisticated, it defines
+the interface that can be used by other packages to talk about trees.
+"""
 module AbstractTrees
 
 export print_tree, TreeCharSet, TreeIterator, Leaves, PostOrderDFS, Tree,
@@ -13,11 +19,6 @@ abstract type AbstractShadowTree end
 
 include("traits.jl")
 include("implicitstacks.jl")
-
-# This package is intended to provide an abstract interface for working
-# with tree structures.
-# Though the package itself is not particularly sophisticated, it defines
-# the interface that can be used by other packages to talk about trees.
 
 """
     children(x)
