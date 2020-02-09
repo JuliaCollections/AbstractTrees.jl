95c2662f9dcc4f7b4f4d02dece5edb5fb24f2277
Tim Holy <tim.holy@gmail.com>
2020-02-09 08:54:31 -0600

    Deploy the documentation

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,13 @@ makedocs(
     modules = [AbstractTrees],
     sitename = "AbstractTrees.jl",
     authors = "Keno Fischer",
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     pages = [
         "index.md",
         "api.md",
     ],
+)
+
+deploydocs(
+    repo = "github.com/JuliaCollections/AbstractTrees.jl.git",
 )
