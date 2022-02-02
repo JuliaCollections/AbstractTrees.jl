895486c830412ff3ccf2d775d8a1b501527a2238
oscarddssmith <oscar.smith@juliacomputing.com>
2022-02-02 11:24:24 -0500

    refactor fixed ambiguity

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,7 @@ import Base: ==
 
 if v"1.1.0" < VERSION # requires https://github.com/JuliaLang/julia/pull/30291
     @testset "Ambiguities" begin
-        if  VERSION < v"1.8-"
-            @test isempty(detect_ambiguities(AbstractTrees, Base, Core))
-        else
-            @test_broken isempty(detect_ambiguities(AbstractTrees, Base, Core))
-        end
+        @test isempty(detect_ambiguities(AbstractTrees, Base, Core))
     end
 end
 
