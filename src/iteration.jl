022b3e9b8ea858cb30269174d8ed93140472ca12
Expanding Man <savastio@protonmail.com>
2022-04-25 19:01:42 -0400

    PreOrderDFS with cursors seems to be working

--- src/iteration.jl
+++ src/iteration.jl
@@ -8,16 +8,48 @@ struct PreOrderDFS{T} <: TreeIterator{T}
     root::T
 end
 
-#FIX: no no no, this isn't even the right protocol
+struct PreOrderState{T}
+    cursor::T
+    visited_children::Bool
 
-function nextcursor(::PreOrderDFS, csr::TreeCursor)
-    n = nextsibling(csr)
-    isnothing(n) || return n
-    ch = children(csr)
-    isempty(ch) ? nothing : first(ch)
+    PreOrderState(csr::TreeCursor, vc::Bool=false) = new{typeof(csr)}(csr, vc)
 end
 
-function Base.iterate(ti::PreOrderDFS, csr::Union{Nothing,TreeCursor}=TreeCursor(ti.root))
-    isnothing(csr) && return nothing
-    (unwrap(csr), nextcursor(ti, csr))
+function _firstchild(s::PreOrderState)
+    ch = children(s.cursor)
+    isempty(ch) ? nothing : PreOrderState(first(ch))
+end
+
+#TODO: this seems to be working but is very confusing, can it be cleaned up?
+function next(s::PreOrderState)
+    n = nextsibling(s.cursor)
+
+    if isnothing(n)
+        if s.visited_children
+            p = parent(s.cursor)
+            isnothing(p) ? nothing : next(PreOrderState(p, true))
+        else
+            o = _firstchild(s)
+            p = parent(s.cursor)
+            if isnothing(o)
+                isnothing(p) ? nothing : next(PreOrderState(p, true))
+            else
+                o
+            end
+        end
+    else
+        if s.visited_children
+            PreOrderState(n)
+        else
+            o = _firstchild(s)
+            isnothing(o) ? PreOrderState(n) : o
+        end
+    end
+end
+
+function Base.iterate(ti::PreOrderDFS,
+                      s::Union{Nothing,PreOrderState}=PreOrderState(TreeCursor(ti.root)),
+                     )
+    isnothing(s) && return nothing
+    (unwrap(s.cursor), next(s))
 end
