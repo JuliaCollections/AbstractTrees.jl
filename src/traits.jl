b19fe40043d49e11d4239d5614d0f2b51b646f2f
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-07-29 21:25:49 +1200

    Update traits.jl

--- src/traits.jl
+++ src/traits.jl
@@ -92,12 +92,15 @@ SiblingLinks(tree) = SiblingLinks(typeof(tree))
     ChildIndexing(node)
 
 A trait indicating whether the tree node `n` has children (as returned by [`children`](@ref)) which can be
-indexed using 1-based indexing.
+indexed using 1-based indexing. Options are either [`NonIndexedChildren`](@ref) (default) or [`IndexedChildren`](@ref).
+
+To declare that the tree `TreeType` supports one-based indexing on the children, define
+```julia
+AbstractTrees.ChildIndexing(::Type{<:TreeType}) = AbstractTrees.IndexedChildren()
+```
 
 If a node has the `IndexedChildren()` so must all connected nodes in the tree.  Otherwise, use
 `NonIndexedChildren()` instead.
-
-Options are either [`NonIndexedChildren`](@ref) (default) or [`IndexedChildren`](@ref).
 """
 abstract type ChildIndexing end
 
