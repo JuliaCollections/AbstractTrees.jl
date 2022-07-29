b4dd00c11036893384be8c3cfb5fca1a7773cf04
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-07-29 21:21:10 +1200

    Update traits.jl

--- src/traits.jl
+++ src/traits.jl
@@ -6,7 +6,7 @@
 A trait which indicates whether a tree node stores references to its parents (`StoredParents()`) or
 if the parents must be inferred from the tree structure (`ImplicitParents()`).
 
-Trees for which `parentlinks` returns `StoredParents()` *MUST* implement [`parent`](@ref).
+Trees for which `ParentLinks` returns `StoredParents()` *MUST* implement [`parent`](@ref).
 
 If `StoredParents()`, all nodes in the tree must also have `StoredParents()`, otherwise use
 `ImplicitParents()`.
